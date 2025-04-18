ea953e3f3718accb69059b58bcc0696a36c53233
Mainak-Kundu00 <smnath2000@gmail.com>
2025-04-18 21:34:57 +0530

    home page connect with codeigniter

--- Project/app/Config/Routes.php
+++ Project/app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('about', 'Home::about');
+$routes->get('rings', 'Home::rings');
+$routes->get('necklaces', 'Home::necklaces');
+$routes->get('jewelry', 'Home::jewelry');
--- Project/app/Controllers/Home.php
+++ Project/app/Controllers/Home.php
@@ -4,8 +4,24 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        return view('index');
+    }
+    public function about()
+    {
+        return view('about');
+    }
+    public function rings()
+    {
+        return view('rings');
+    }
+    public function necklaces()
+    {
+        return view('necklaces');
+    }
+    public function jewelry()
+    {
+        return view('jewelry');
     }
 }
