42695c857cedd3c9d64baf0010c542f2eaf4bdfe
Mainak-Kundu00 <smnath2000@gmail.com>
2025-05-09 17:58:14 +0530

    Work on sign in

--- Project/app/Controllers/Create_user.php
+++ Project/app/Controllers/Create_user.php
@@ -8,16 +8,40 @@ use App\Models\Users_model;
 class Create_user extends BaseController{
     protected $model;
 
+    protected $array;
+ 
     public function index(){
+
+        // $array= [
+        //     "name"=>'mainak',
+        // ];
+        // $this->session->set($array);
+        // $name = $this->session->get('name');
+        
         $this->model= model(Users_model::class);
         //checking function to know model is connected or not
-        $this->model->hello();
+        //$this->model->hello();
         echo"<br>";
 
         $user_data=$this->request->getPost();
         //checking to know wherther data is flowing or not 
         print_r($user_data);
         echo"<pre>" . "Form Submitted";
+        //. $name;
+
+        $name=$user_data['name'];
+        $password=$user_data['password'];
+        //echo $name ." and ". $password;
+        //if(
+          //$return=
+          $this->model->user_login($name,$password);
+          //echo $return;
+        //     ){
+        //     return view("index");
+        // }
+        // else{
+        //     return redirect()->to('sign_up');
+        // }
         
     }
 
--- Project/app/Models/Users_model.php
+++ Project/app/Models/Users_model.php
@@ -2,12 +2,38 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
-
+use Config\Database;
 
 class Users_model extends Model{
 
+    protected $db;
+    protected $users;
+
+    public function __construct(){
+        $this->db = Database::connect();
+        $this->users = $this->db->table('users');
+        $this->db->connect();
+
+    }
+
     public function hello(){
         echo "IN the model class";
+        $query   = $this->users->get();
+        foreach($query->getresult('array') as $row){
+            echo $row['name'];//gives name from database user table
+        }
+        
+    }
+
+    public function user_login($name,$password){
+         $query= $this->users->select('id')
+                ->where(['name'=>$name,'password'=>$password])
+                ->get()
+                ->getresult('array');
+        
+         print_r($query);
+        // echo $query->getnumrows();
+       // echo $query[0];
     }
 
 }
